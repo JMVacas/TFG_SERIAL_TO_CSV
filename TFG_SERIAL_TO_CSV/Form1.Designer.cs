83a1f513176b6c5028ee353440835a03ab7b19e7
Jorge Martin <porcelanaskobayashi@outlook.es>
2018-09-09 22:39:12 +0200

    Agregar archivos de proyecto.

---
+++ TFG_SERIAL_TO_CSV/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace TFG_SERIAL_TO_CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Puerto_Serie = new System.IO.Ports.SerialPort(this.components);
+            this.Start = new System.Windows.Forms.Button();
+            this.Input_Iteraciones = new System.Windows.Forms.TextBox();
+            this.PuertoCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BaudRateCombo = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Parar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Input_Ajuste = new System.Windows.Forms.TextBox();
+            this.Barra_De_Progreso = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // Puerto_Serie
+            // 
+            this.Puerto_Serie.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.Puerto_Serie_DataReceived);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(78, 132);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(65, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Empezar";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Input_Iteraciones
+            // 
+            this.Input_Iteraciones.Location = new System.Drawing.Point(96, 66);
+            this.Input_Iteraciones.MaxLength = 1;
+            this.Input_Iteraciones.Name = "Input_Iteraciones";
+            this.Input_Iteraciones.Size = new System.Drawing.Size(36, 20);
+            this.Input_Iteraciones.TabIndex = 2;
+            this.Input_Iteraciones.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // PuertoCombo
+            // 
+            this.PuertoCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PuertoCombo.FormattingEnabled = true;
+            this.PuertoCombo.Location = new System.Drawing.Point(67, 12);
+            this.PuertoCombo.Name = "PuertoCombo";
+            this.PuertoCombo.Size = new System.Drawing.Size(65, 21);
+            this.PuertoCombo.TabIndex = 0;
+            this.PuertoCombo.DropDown += new System.EventHandler(this.PuertoCombo_DropDown);
+            this.PuertoCombo.SelectedIndexChanged += new System.EventHandler(this.Seleccion_Puerto_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Puerto";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "BaudRate";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // BaudRateCombo
+            // 
+            this.BaudRateCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.BaudRateCombo.FormattingEnabled = true;
+            this.BaudRateCombo.Location = new System.Drawing.Point(67, 39);
+            this.BaudRateCombo.Name = "BaudRateCombo";
+            this.BaudRateCombo.Size = new System.Drawing.Size(65, 21);
+            this.BaudRateCombo.TabIndex = 4;
+            this.BaudRateCombo.SelectedIndexChanged += new System.EventHandler(this.BaudRateCombo_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Iteraciones";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Parar
+            // 
+            this.Parar.Location = new System.Drawing.Point(7, 132);
+            this.Parar.Name = "Parar";
+            this.Parar.Size = new System.Drawing.Size(65, 23);
+            this.Parar.TabIndex = 9;
+            this.Parar.Text = "Parar";
+            this.Parar.UseVisualStyleBackColor = true;
+            this.Parar.Click += new System.EventHandler(this.Parar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Ajuste";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Input_Ajuste
+            // 
+            this.Input_Ajuste.Location = new System.Drawing.Point(96, 93);
+            this.Input_Ajuste.MaxLength = 4;
+            this.Input_Ajuste.Name = "Input_Ajuste";
+            this.Input_Ajuste.Size = new System.Drawing.Size(36, 20);
+            this.Input_Ajuste.TabIndex = 10;
+            this.Input_Ajuste.TextChanged += new System.EventHandler(this.Input_Ajuste_TextChanged);
+            // 
+            // Barra_De_Progreso
+            // 
+            this.Barra_De_Progreso.Location = new System.Drawing.Point(9, 163);
+            this.Barra_De_Progreso.Name = "Barra_De_Progreso";
+            this.Barra_De_Progreso.Size = new System.Drawing.Size(134, 23);
+            this.Barra_De_Progreso.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(155, 193);
+            this.Controls.Add(this.Barra_De_Progreso);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Input_Ajuste);
+            this.Controls.Add(this.Parar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BaudRateCombo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Input_Iteraciones);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.PuertoCombo);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort Puerto_Serie;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox Input_Iteraciones;
+        private System.Windows.Forms.ComboBox PuertoCombo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox BaudRateCombo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Parar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Input_Ajuste;
+        private System.Windows.Forms.ProgressBar Barra_De_Progreso;
+    }
+}
+
---
+++ TFG_SERIAL_TO_CSV/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TFG_SERIAL_TO_CSV
+{
+
+    public partial class Form1 : Form
+    {
+        string[] Port_Names;
+        string[] BaudRateItems = { "300", "600", "1200", "2400", "4800", "9600", "14400", "19200"
+                , "22800", "38400", "57600", "115200" };
+        string[] Colores = { "IR_970", "IR_850", "Rojo", "Naranja", "Amarillo", "Verde", "Azul", "UV" };
+        int Progreso = 0;
+        string Data_Received;   // Aqui se guarda un string temporal con todos los datos recibidos
+        delegate void StringArgReturningVoidDelegate(string text);
+        delegate void IntArgReturningVoidDelegate(int Value);
+        delegate void ShowMessageDelegate(IWin32Window win32Window, string Texto, string Titulo, MessageBoxButtons messageBoxButtons, MessageBoxIcon messageBoxIcon);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BaudRateCombo.Items.AddRange(BaudRateItems);
+            MessageBox.Show()
+        }
+
+
+        private void Seleccion_Puerto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PuertoCombo_DropDown(object sender, EventArgs e)
+        {
+            
+            Port_Names = SerialPort.GetPortNames();
+            PuertoCombo.Items.Clear();
+            PuertoCombo.Items.AddRange(Port_Names);
+        }
+
+        private void BaudRateCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        //Desencadena el inicio de la recepcion de datos
+        private void Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Puerto_Serie.Close();
+                Puerto_Serie.PortName = PuertoCombo.SelectedItem.ToString();
+                Puerto_Serie.BaudRate = Convert.ToInt32(BaudRateCombo.SelectedItem);
+                Puerto_Serie.Open();
+                if (Puerto_Serie.IsOpen)
+                Puerto_Serie.Write("Start");
+                Puerto_Serie.DiscardInBuffer();
+                Data_Received = "";
+            }
+            catch (Exception error)
+            {
+
+            }
+        }
+        //Evento que ocurre cada vez que se reciben Datos por UART
+        private void Puerto_Serie_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string Temporal = "";
+            Temporal = Puerto_Serie.ReadLine();
+            if (Data_Received.Contains("Iteraciones"))
+            {
+                Puerto_Serie.Write(Input_Iteraciones.Text);
+            }
+            else if (Data_Received.Contains("Ajuste"))
+            {
+                Puerto_Serie.Write(Input_Ajuste.Text);
+            }
+            else if (Data_Received.Contains("Progreso"))
+            {
+                Set_Progreso(Convert.ToInt32(Regex.Match(Temporal, @"\d")));                   
+            }
+            else if (Data_Received.Contains("Finished"))
+            {
+                SetText("Guardar");
+                ShowMessageBox(this, "El proceso ha finalizado correctamente", "Finalizado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        //Cierra el puerto serie
+        private void Parar_Click(object sender, EventArgs e)
+        {
+            Puerto_Serie.Write("Stop");
+            Puerto_Serie.Close();
+            if (Data_Received.Contains("Finished"))
+            {
+                Explorer.Guardar(Data_Received);
+                Parar.Text = "Parar";
+            }
+
+        }
+        //Se escribe el numero de Iteraciones
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+               Convert.ToInt32(Input_Iteraciones.Text);
+            }
+            catch (Exception Error)
+            {
+                MessageBox.Show(this, "El valor introducido no es un numero" ,"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Input_Iteraciones.Text = "0";
+            }
+        }
+        
+        private void SetText(string text)
+        {
+            //InvokeRequired compara la ID del hilo es igual que la ID del hilo que la creo,
+            //si son diferentes retorna true, es necesario ya que al abrir el puerto COM
+            //se instancia un nuevo hilo, si se intenta cambiar el texto desde el captador de 
+            // eventos del puerto COM, da un error ya que el hilo es distinto al hilo que lo creo,
+            //poir lo tanto esta funcion, se utiliza esta funcion 
+            //como delegado para poder modificar el texto.
+            if (Parar.InvokeRequired)
+            {
+                StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(SetText);
+                Invoke(d, new object[] { text });
+            }
+            else
+            {
+                Parar.Text = text;
+            }
+        }
+
+        private void Input_Ajuste_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Convert.ToDouble(Input_Ajuste.Text);
+            }
+            catch (Exception Error)
+            {
+                MessageBox.Show(this, "El valor introducido no es un numero, recuerde que solo admite comas para introducir los decimales", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Input_Ajuste.Text = "0";
+            }
+        }
+        private void Set_Progreso(int Value)
+        {
+            //Al igual que el metodo SetText, para cambiar el progreso desde la funcion de 
+            //PuertoSerie_Data_Received es necesario utilizar delegados
+            if (Barra_De_Progreso.InvokeRequired)
+            {
+                IntArgReturningVoidDelegate d = new IntArgReturningVoidDelegate(Set_Progreso);
+                Invoke(d, new object[] { Value });
+            }
+            else
+            {
+                Barra_De_Progreso.Value = Value;
+            }
+        }
+        private void ShowMessageBox(IWin32Window win32Window, string Texto, string Titulo, MessageBoxButtons messageBoxButtons, MessageBoxIcon messageBoxIcon)
+        {
+            //Al igual que el metodo SetText, para mostrar el final del proceso desde la funcion de
+            //PuertoSerie_Data_Received es necesario utilizar delegados
+            if (Barra_De_Progreso.InvokeRequired)
+            {
+                IntArgReturningVoidDelegate d = new IntArgReturningVoidDelegate(Set_Progreso);
+                Invoke(d, new object[] { win32Window, Texto, Titulo, messageBoxButtons, messageBoxIcon });
+            }
+            else
+            {
+                MessageBox.Show(win32Window, Texto, Titulo, messageBoxButtons, messageBoxIcon);
+            }
+        }
+    }
+}
